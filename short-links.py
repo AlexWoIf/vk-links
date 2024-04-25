3dd70f91a07738b573721f7328a586398c543c2c
AlexWoIf <alexey.barannikov@gmail.com>
2024-04-25 21:34:51 +0300

    separate code in the function

--- short-links.py
+++ short-links.py
@@ -8,13 +8,17 @@ URL = 'https://api.vk.com/method/'
 METHOD = 'utils.getShortLink'
 
 
+def shorten_link(token, url):
+    headers = {"Authorization": f"Bearer {vk_api_key}", }
+    params = {"v": '5.199', 'url': target_url}
+    response = requests.get(url=URL+METHOD, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json().get('response').get('short_url')
+
+
 if __name__ == '__main__':
     load_dotenv(override=True)
     vk_api_key = os.getenv('VK_API_KEY')
 
     target_url = 'https://dvmn.org/modules'
-    headers = {"Authorization": f"Bearer {vk_api_key}", }
-    params = {"v": '5.199', 'url': target_url}
-    response = requests.get(url=URL+METHOD, headers=headers, params=params)
-    response.raise_for_status()
-    print(f'{response.json()=}')
+    print(f'Сокращенная ссылка: {shorten_link(vk_api_key, target_url)}')
