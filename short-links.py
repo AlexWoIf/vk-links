ed0270f54a363751568405dee3519339a043c692
AlexWoIf <alexey.barannikov@gmail.com>
2024-04-25 22:36:26 +0300

    add error handling

--- short-links.py
+++ short-links.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import requests
 
@@ -13,12 +14,26 @@ def shorten_link(token, url):
     params = {"v": '5.199', 'url': target_url}
     response = requests.get(url=URL+METHOD, headers=headers, params=params)
     response.raise_for_status()
-    return response.json().get('response').get('short_url')
+    answer = response.json()
+    error = answer.get('error')
+    if error:
+        raise requests.exceptions.HTTPError(
+                URL+METHOD, 400, "Wrong data provided", response=answer
+              )
+    return answer.get('response').get('short_url')
 
 
 if __name__ == '__main__':
     load_dotenv(override=True)
     vk_api_key = os.getenv('VK_API_KEY')
 
-    target_url = 'https://dvmn.org/modules'
-    print(f'Сокращенная ссылка: {shorten_link(vk_api_key, target_url)}')
+    parser = argparse.ArgumentParser(
+        description='Укажите ссылку, которую нужно сократить.')
+    parser.add_argument('target_url', type=str,
+                        help='Target url')
+    args = parser.parse_args()
+    try:
+        target_url = args.target_url
+        print(f'Сокращенная ссылка: {shorten_link(vk_api_key, target_url)}')
+    except requests.exceptions.HTTPError as exc:
+        print(exc)
